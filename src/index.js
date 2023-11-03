656c26df915a15d2a06795e3c1df9ba634a77395
Kate <kate@MacBook-Pro-avens.local>
2023-11-03 13:55:41 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
-// eslint-disable-next-line no-unused-vars
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './components/app';
 
 const root = document.getElementById('root');
-//
 
-//
 createRoot(root).render(<App />);
